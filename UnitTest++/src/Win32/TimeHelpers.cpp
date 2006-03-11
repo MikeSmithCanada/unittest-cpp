569af74a462d94633e52818fb9ffa7f41939c4b4
llopis <llopis@feadf2a4-a60d-0410-a25d-8df00a7d1702>
2006-03-11 19:00:44 +0000

    VS2005 project and solution. Project building and running, but failing on timings.
    git-svn-id: https://unittest-cpp.svn.sourceforge.net/svnroot/unittest-cpp@37 feadf2a4-a60d-0410-a25d-8df00a7d1702

---
+++ UnitTest++/src/Win32/TimeHelpers.cpp
@@ -0,0 +1,19 @@
+#include "TimeHelpers.h"
+
+namespace UnitTest {
+
+ScopedTimer::ScopedTimer()
+{
+}
+
+int ScopedTimer::GetTimeInMs() const
+{
+	return 0;
+}
+
+
+void TimeHelpers::SleepMs (int ms)
+{
+}
+
+}
---
+++ UnitTest++/src/Win32/TimeHelpers.h
@@ -0,0 +1,26 @@
+#ifndef UNITTEST_TIMEHELPERS_H
+#define UNITTEST_TIMEHELPERS_H
+
+namespace UnitTest {
+
+class ScopedTimer
+{
+public:
+    ScopedTimer();
+    int GetTimeInMs() const;    
+
+private:
+};
+
+
+namespace TimeHelpers
+{
+void SleepMs (int ms);
+}
+
+
+}
+
+
+
+#endif
