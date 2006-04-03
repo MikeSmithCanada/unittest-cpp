154c9e94fef423e708f56da71c42aa5d5cf0593b
llopis <llopis@feadf2a4-a60d-0410-a25d-8df00a7d1702>
2006-04-03 04:27:39 +0000

    Added BSD to posix list. Changed -Wextra to -W for compatibility with older gcc version.
    git-svn-id: https://unittest-cpp.svn.sourceforge.net/svnroot/unittest-cpp@118 feadf2a4-a60d-0410-a25d-8df00a7d1702

--- UnitTest++/src/Config.h
+++ UnitTest++/src/Config.h
@@ -13,7 +13,8 @@
 #endif
 
 
-#if defined(unix) || defined(__unix__) || defined(__unix) || defined(linux) || defined(__APPLE__)
+#if defined(unix) || defined(__unix__) || defined(__unix) || defined(linux) || \
+    defined(__APPLE__) || defined (__NetBSD__) || defined (__OpenBSD__) || defined (__FreeBSD__)
     #define UNITTEST_POSIX
 #endif
 
